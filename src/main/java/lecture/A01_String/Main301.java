004a95370fc2155c4bbfc7a4588c1442aa0a4097
jinsu291 <wlstn291@naver.com>
2022-09-16 09:45:46 +0900

    Main301 추가

---
+++ src/main/java/lecture/A01_String/Main301.java
@@ -0,0 +1,26 @@
+package lecture.A01_String;
+
+import java.util.Scanner;
+// 다시 보기!!
+public class Main301 {
+    public String solution(String str){
+    String answer = "";
+    int m = Integer.MIN_VALUE;
+    String [] s = str.split(" ");
+    for (String x : s){
+        int len = x.length();
+        if(len > m){
+            m = len;
+            answer = x;
+        }
+    }
+    return answer;
+    }
+
+    public static void main(String[] args) {
+        Main301 T = new Main301();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        System.out.println(T.solution(str));
+    }
+}
