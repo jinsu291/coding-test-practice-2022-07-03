37c3551a790e1cbf04e370b30359e3ae9f20dde9
jinsu291 <wlstn291@naver.com>
2022-09-16 10:10:53 +0900

    Main401 추가

---
+++ src/main/java/lecture/A01_String/Main401.java
@@ -0,0 +1,28 @@
+package lecture.A01_String;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+// 어려움 다시보기!!
+public class Main401 {
+    public ArrayList<String> solution(int n, String[] str){
+        ArrayList<String> answer = new ArrayList<>();
+        for(String x : str){
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main401 T = new Main401();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for(int i = 0; i < n; i++ ){
+            str[i] = kb.next();
+        }
+        for(String x : T.solution(n, str)){
+            System.out.println(x);
+        }
+    }
+}
