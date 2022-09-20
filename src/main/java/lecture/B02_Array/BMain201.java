35c3296b6ca1a48da6521dbf6a49679d61b6201c
jinsu291 <wlstn291@naver.com>
2022-09-20 15:41:35 +0900

    BMain201 추가

---
+++ src/main/java/lecture/B02_Array/BMain201.java
@@ -0,0 +1,28 @@
+package lecture.B02_Array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// 어려움 다시보기!!
+public class BMain201 {
+    public int solution(int n, int[] arr){
+        int answer = 1, max = arr[0];
+        for(int i = 1; i < n; i++){
+            if(arr[i] > max) {
+                max = arr[i];
+                answer++;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        BMain201 T = new BMain201();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = kb.nextInt();
+        }
+            System.out.println(T.solution(n, arr));
+    }
+}
