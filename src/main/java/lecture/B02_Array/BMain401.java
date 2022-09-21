afef40038335649174506d9a6f7eda97628fbafc
jinsu291 <wlstn291@naver.com>
2022-09-21 17:07:57 +0900

    BMain401 추가

---
+++ src/main/java/lecture/B02_Array/BMain401.java
@@ -0,0 +1,25 @@
+package lecture.B02_Array;
+
+import java.util.Scanner;
+
+// 어려움 다시보기!!
+public class BMain401 {
+    public int[] solution(int n){
+        int[] answer = new int[n];
+        answer[0] = 1;
+        answer[1] = 1;
+        for(int i = 2; i < n; i++){
+            answer[i] = answer[i - 1] + answer[i - 2];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        BMain401 T = new BMain401();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        for (int x : T.solution(n)){
+            System.out.print(x + " ");
+        }
+    }
+}
