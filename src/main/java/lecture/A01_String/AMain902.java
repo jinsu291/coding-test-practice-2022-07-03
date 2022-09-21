80764fa5f9103e79d720c33c08abd0795d48776c
jinsu291 <wlstn291@naver.com>
2022-09-21 09:48:42 +0900

    AMain902 추가

---
+++ src/main/java/lecture/A01_String/AMain902.java
@@ -0,0 +1,23 @@
+package lecture.A01_String;
+
+import java.util.Scanner;
+
+// 다시보기!!
+public class AMain902 {
+    public int solution(String s) {
+        int answer = 0;
+        for(char x : s.toCharArray()){
+            if(x >= 48 && x <= 57){
+                answer = answer * 10 + (x-48);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        AMain902 T = new AMain902();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+}
