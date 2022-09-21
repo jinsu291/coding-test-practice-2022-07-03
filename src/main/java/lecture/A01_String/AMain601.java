a5d27ed76c7389d625ad05598a61a185cd7711aa
jinsu291 <wlstn291@naver.com>
2022-09-21 09:08:58 +0900

    AMain601 내용 추가

--- src/main/java/lecture/A01_String/AMain601.java
+++ src/main/java/lecture/A01_String/AMain601.java
@@ -7,6 +7,7 @@ public class AMain601 {
     public String solution(String str) {
         String answer ="";
         for(int i = 0; i < str.length(); i++ ){
+            //System.out.println(str.charAt(i)+" "+i+" "+str.indexOf(str.charAt(i)));
             if(str.indexOf(str.charAt(i)) == i){
                 answer += str.charAt(i);
             }
