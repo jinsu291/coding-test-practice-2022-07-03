4498325fba3db63d21d42d8ab92301266ddbd50e
jinsu291 <wlstn291@naver.com>
2022-09-21 11:32:16 +0900

    AMain1001 추가

---
+++ src/main/java/lecture/A01_String/AMain1001.java
@@ -0,0 +1,40 @@
+package lecture.A01_String;
+
+import java.util.Scanner;
+
+// 다시보기!!
+public class AMain1001 {
+    public int[] solution(String s, char t) {
+        int[] answer = new int[s.length()];
+        int p = 1000;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == t){
+                p = 0;
+                answer[i] = p;
+            } else {
+                p++;
+                answer[i] = p;
+            }
+        }
+        p = 1000;
+        for(int i = s.length() - 1; i >= 0; i--){
+            if (s.charAt(i) == t){
+                p = 0;
+            } else {
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        AMain1001 T = new AMain1001();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+        for (int x : T.solution(str, c)){
+            System.out.println(x + " ");
+        }
+    }
+}
