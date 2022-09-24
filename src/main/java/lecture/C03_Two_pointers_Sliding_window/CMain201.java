7fde683a197f0ef2dbf7cde2ab251e754f3cd046
jinsu291 <wlstn291@naver.com>
2022-09-24 14:26:41 +0900

    CMain201 추가

---
+++ src/main/java/lecture/C03_Two_pointers_Sliding_window/CMain201.java
@@ -0,0 +1,39 @@
+package lecture.C03_Two_pointers_Sliding_window;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 어려움 다시보기!!
+public class CMain201 {
+    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b){
+        ArrayList<Integer> answer = new ArrayList<>();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int p1 = 0, p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (a[p1] == b[p2]){
+                answer.add(a[p1++]);
+                p2++;
+            } else if (a[p1] < b[p2]){
+                p1++;
+            } else p2++;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        CMain201 T = new CMain201();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = kb.nextInt();
+        }
+        int m = kb.nextInt();
+        int[] b = new int[m];
+        for(int i = 0; i<m; i++){
+            b[i] = kb.nextInt();
+        }
+        for(int x : T.solution(n, m, a, b)) System.out.print(x+" ");
+    }
+}
