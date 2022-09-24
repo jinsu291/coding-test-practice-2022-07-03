dcf22a627715a533112150ad780fb1321aaab60a
jinsu291 <wlstn291@naver.com>
2022-09-24 14:09:14 +0900

    BMain801 추가

---
+++ src/main/java/lecture/B02_Array/BMain801.java
@@ -0,0 +1,33 @@
+package lecture.B02_Array;
+
+import java.util.Scanner;
+
+// 어려움 다시보기!!
+public class BMain801 {
+    public int[] solution(int n, int[] arr){
+        int[] answer = new int[n];
+        for(int i = 0; i < n; i++){
+            int cnt = 1;
+            for(int j = 0; j < arr.length; j++){
+                if(arr[j] > arr[i]) {
+                    cnt++;
+                }
+            }
+            answer[i] = cnt;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        BMain801 T = new BMain801();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = kb.nextInt();
+        }
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
