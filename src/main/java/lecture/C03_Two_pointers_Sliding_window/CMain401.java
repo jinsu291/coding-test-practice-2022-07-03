324fae0b9cfef2afe7af04ddfb082fa9ace63ef1
jinsu291 <wlstn291@naver.com>
2022-09-25 23:00:49 +0900

    CMain401 추가

---
+++ src/main/java/lecture/C03_Two_pointers_Sliding_window/CMain401.java
@@ -0,0 +1,34 @@
+package lecture.C03_Two_pointers_Sliding_window;
+
+import java.util.Scanner;
+
+// 어려움 다시보기!!
+public class CMain401 {
+    public int solution(int n, int m, int[] arr){
+        int answer = 0, sum = 0, lt = 0;
+        for(int rt = 0; rt < n; rt++){
+            sum += arr[rt];
+            if(sum == m) {
+                answer++;
+            }
+            while (sum >= m){
+                sum -= arr[lt++];
+                if(sum ==m ){
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        CMain401 T = new CMain401();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(T.solution(n, m, arr));
+    }
+}
