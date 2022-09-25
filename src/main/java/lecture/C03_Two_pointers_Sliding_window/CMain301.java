33bcba44b6ac01b18d68d654a7028d673822deb6
jinsu291 <wlstn291@naver.com>
2022-09-25 22:43:48 +0900

    CMain301 추가

---
+++ src/main/java/lecture/C03_Two_pointers_Sliding_window/CMain301.java
@@ -0,0 +1,32 @@
+package lecture.C03_Two_pointers_Sliding_window;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 어려움 다시보기!!
+public class CMain301 {
+    public int solution(int n, int k, int[] arr){
+        int answer, sum = 0;
+        for(int i = 0; i < k; i++){
+            sum += arr[i];
+        }
+        answer = sum;
+        for(int i = k; i < n; i++ ){
+            sum += (arr[i] - arr[i - k]);
+            answer = Math.max(answer, sum);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        CMain301 T = new CMain301();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(T.solution(n, k, arr));
+    }
+}
