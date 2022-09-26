928aff5de820649cdb9f7d7557b6176532c96058
jinsu291 <wlstn291@naver.com>
2022-09-27 01:42:46 +0900

    DMain501 추가

---
+++ src/main/java/lecture/D04_HashMap_HashSet_TreeSet/DMain501.java
@@ -0,0 +1,41 @@
+package lecture.D04_HashMap_HashSet_TreeSet;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+// 어려움 다시보기!!
+public class DMain501 {
+    public int solution(int[] arr, int n, int k){
+       int answer = -1;
+        TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for(int l = j+ 1; l < n; l++) {
+                    Tset.add(arr[i] + arr[j] + arr[l]);
+                }
+            }
+        }
+        int cnt = 0;
+        for (int x : Tset) {
+            cnt++;
+            if (cnt ==k){
+                return x;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        DMain501 T = new DMain501();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(T.solution(arr, n, k));
+    }
+}
