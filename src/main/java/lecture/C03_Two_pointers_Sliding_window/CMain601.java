1fea3477b9740b490ac3ce45c0bb73e7b80534f8
jinsu291 <wlstn291@naver.com>
2022-09-26 09:34:36 +0900

    CMain601 추가

---
+++ src/main/java/lecture/C03_Two_pointers_Sliding_window/CMain601.java
@@ -0,0 +1,35 @@
+package lecture.C03_Two_pointers_Sliding_window;
+
+import java.util.Scanner;
+
+// 어려움 다시보기!!
+public class CMain601 {
+    public int solution(int n, int k, int[] arr){
+        int answer = 0, cnt = 0, lt =0;
+        for(int rt = 0; rt < n; rt++){
+            if(arr[rt] == 0){
+                cnt++;
+            }
+            while (cnt > k){
+                if (arr[lt] == 0){
+                    cnt--;
+                }
+                lt++;
+            }
+            answer = Math.max(answer, rt - lt + 1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        CMain601 T = new CMain601();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(T.solution(n, k, arr));
+    }
+}
