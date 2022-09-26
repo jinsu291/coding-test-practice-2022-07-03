bb13eaec7401e63eacd6d3c8711628a5c2adf631
jinsu291 <wlstn291@naver.com>
2022-09-26 09:22:45 +0900

    CMain502 추가

---
+++ src/main/java/lecture/C03_Two_pointers_Sliding_window/CMain502.java
@@ -0,0 +1,26 @@
+package lecture.C03_Two_pointers_Sliding_window;
+
+import java.util.Scanner;
+
+// 어려움 다시보기!!
+public class CMain502 {
+    public int solution(int n){
+        int answer = 0, cnt = 1;
+        n--;
+        while(n > 0) {
+            cnt++;
+            n = n - cnt;
+            if(n % cnt == 0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        CMain502 T = new CMain502();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        System.out.println(T.solution(n));
+    }
+}
