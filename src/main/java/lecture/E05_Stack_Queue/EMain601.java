8e8cee96a8047381b9632908b7411af2d0b721e3
jinsu291 <wlstn291@naver.com>
2022-09-27 11:14:15 +0900

    EMain601 추가

---
+++ src/main/java/lecture/E05_Stack_Queue/EMain601.java
@@ -0,0 +1,35 @@
+package lecture.E05_Stack_Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+// 어려움 다시보기!!
+public class EMain601 {
+    public int solution(int n, int k){
+        int answer = 0;
+        Queue<Integer> Q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            Q.offer(i);
+        }
+        while (!Q.isEmpty()){
+            for (int i = 1; i < k; i++){
+                Q.offer(Q.poll());
+            }
+                Q.poll();
+                if (Q.size() == 1){
+                    answer = Q.poll();
+                }
+            }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        EMain601 T = new EMain601();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        System.out.println(T.solution(n, k));
+    }
+}
