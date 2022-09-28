233365b9c84329070539e5f4a631c2babc804668
jinsu291 <wlstn291@naver.com>
2022-09-28 15:21:08 +0900

    FMain101 추가

---
+++ src/main/java/lecture/F06_Sorting_and_Searching/FMain101.java
@@ -0,0 +1,35 @@
+package lecture.F06_Sorting_and_Searching;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+// 선택정렬
+public class FMain101 {
+    public int[] solution(int n, int[] arr){
+        for (int i = 0; i < n - 1; i++){
+            int idx = i;
+            for (int j = i + 1; j < n; j++){
+                if (arr[j] < arr[idx]) {
+                    idx = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        FMain101 T = new FMain101();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = kb.nextInt();
+        }
+        for (int x : T.solution(n, arr)){
+            System.out.print(x + " ");
+        }
+    }
+}
