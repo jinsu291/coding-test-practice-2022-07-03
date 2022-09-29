c1395229c5ce44c738f1b806925784c9262061fa
jinsu291 <wlstn291@naver.com>
2022-09-30 01:57:44 +0900

    FMain601 추가

---
+++ src/main/java/lecture/F06_Sorting_and_Searching/FMain601.java
@@ -0,0 +1,33 @@
+package lecture.F06_Sorting_and_Searching;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 삽입정렬
+public class FMain601 {
+    public ArrayList<Integer> solution(int n, int[] arr){
+        ArrayList<Integer> answer = new ArrayList<>();
+        int[] tmp = arr.clone();
+        Arrays.sort(tmp);
+        for (int i = 0; i < n; i++){
+            if (arr[i] != tmp[i]){
+                answer.add(i + 1);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FMain601 T = new FMain601();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = kb.nextInt();
+        }
+        for (int x : T.solution(n, arr)){
+            System.out.print(x + " ");
+        }
+    }
+}
