42d27c95a81364be9225cee70f8ada7bda432b2f
jinsu291 <wlstn291@naver.com>
2022-10-04 23:39:14 +0900

    GMain201 추가

---
+++ src/main/java/lecture/G07_Recursive_Tree_Graph/GMain201.java
@@ -0,0 +1,20 @@
+package lecture.G07_Recursive_Tree_Graph;
+
+public class GMain201 {
+    public void DFS(int n){
+        if (n == 0){
+            return;
+        } else {
+            DFS(n / 2);
+            System.out.print(n % 2);
+        }
+    }
+    public void solution(int n){
+        DFS(n);
+    }
+
+    public static void main(String[] args) {
+        GMain201 T = new GMain201();
+        T.solution(11);
+    }
+}
