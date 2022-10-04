324859e28dc7723cc55c364030c45d3c2f33f09e
jinsu291 <wlstn291@naver.com>
2022-10-04 23:42:36 +0900

    GMain301 추가

---
+++ src/main/java/lecture/G07_Recursive_Tree_Graph/GMain301.java
@@ -0,0 +1,16 @@
+package lecture.G07_Recursive_Tree_Graph;
+
+public class GMain301 {
+    public int DFS(int n){
+        if (n == 1){
+            return 1;
+        } else {
+            return n * DFS(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        GMain301 T = new GMain301();
+        System.out.println(T.DFS(5));
+    }
+}
