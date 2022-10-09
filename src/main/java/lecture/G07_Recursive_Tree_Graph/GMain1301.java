be559aa2a73237f534db2a9faf6b4bc20150392e
jinsu291 <wlstn291@naver.com>
2022-10-09 22:00:22 +0900

    GMain1301 추가

--- src/main/java/lecture/G07_Recursive_Tree_Graph/GMain1301.java
+++ src/main/java/lecture/G07_Recursive_Tree_Graph/GMain1301.java
@@ -1,35 +1,39 @@
 package lecture.G07_Recursive_Tree_Graph;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-class GMain1101 {
+class GMain1301 {
     static int n, m, answer = 0;
-    static int[][] graph;
+    static ArrayList<ArrayList<Integer>> graph;
     static int[] ch;
     public void DFS(int v){
         if(v == n) answer++;
         else{
-            for(int i = 1; i <= n; i++){
-                if(graph[v][i] == 1 && ch[i] == 0){
-                    ch[i] = 1;
-                    DFS(i);
-                    ch[i] = 0;
+            for(int nv : graph.get(v)){
+                if(ch[nv] == 0){
+                    ch[nv] = 1;
+                    DFS(nv);
+                    ch[nv] = 0;
                 }
             }
         }
     }
 
     public static void main(String[] args){
-        GMain1101 T = new GMain1101();
+        GMain1301 T = new GMain1301();
         Scanner kb = new Scanner(System.in);
         n = kb.nextInt();
         m = kb.nextInt();
-        graph = new int[n+1][n+1];
-        ch = new int[n+1];
+        graph = new ArrayList<ArrayList<Integer>>();
+        for(int i = 0; i <= n; i++){
+            graph.add(new ArrayList<Integer>());
+        }
+        ch = new int[n + 1];
         for(int i = 0; i < m; i++){
             int a = kb.nextInt();
             int b = kb.nextInt();
-            graph[a][b] = 1;
+            graph.get(a).add(b);
         }
         ch[1] = 1;
         T.DFS(1);
