d80976e46b307b421f8cbcc6080b9522b7cd6842
jinsu291 <wlstn291@naver.com>
2022-10-12 22:09:41 +0900

    HMain601 추가

---
+++ src/main/java/lecture/H08_DFS_BFS/HMain601.java
@@ -0,0 +1,34 @@
+package lecture.H08_DFS_BFS;
+
+import java.util.*;
+class HMain601{
+    static int[] pm, ch, arr;
+    static int n, m;
+    public void DFS(int L){
+        if(L==m){
+            for(int x : pm) System.out.print(x+" ");
+            System.out.println();
+        }
+        else{
+            for(int i=0; i<n; i++){
+                if(ch[i]==0){
+                    ch[i]=1;
+                    pm[L]=arr[i];
+                    DFS(L+1);
+                    ch[i]=0;
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+        HMain601 T = new HMain601();
+        Scanner kb = new Scanner(System.in);
+        n=kb.nextInt();
+        m=kb.nextInt();
+        arr=new int[n];
+        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+        ch=new int[n];
+        pm=new int[m];
+        T.DFS(0);
+    }
+}
