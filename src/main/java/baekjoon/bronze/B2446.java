0466bc66a3844f7f3c8ffeb1d1968d9df81b37be
jinsu291 <wlstn291@naver.com>
2022-10-20 21:10:39 +0900

    B2446 추가

---
+++ src/main/java/baekjoon/bronze/B2446.java
@@ -0,0 +1,29 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class B2446 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int a = kb.nextInt();
+
+        for (int i = 0; i < a - 1; i++){
+            for (int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 2 * (a - i) -1 ; k > 0; k--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int i = 0; i < a; i++){
+            for (int j = 0; j < a - i - 1; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
