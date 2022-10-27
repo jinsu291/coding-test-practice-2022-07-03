a2c3fab4a52d64ab57de3e6d6d5d13acd39a3505
jinsu291 <wlstn291@naver.com>
2022-10-28 02:29:15 +0900

    IMain301 수정

--- src/main/java/lecture/I09_Greedy/IMain201.java
+++ src/main/java/lecture/I09_Greedy/IMain201.java
@@ -1,25 +1,25 @@
 package lecture.I09_Greedy;
 
 import java.util.*;
-class Time implements Comparable<Time>{
+class Time201 implements Comparable<Time201>{
     public int s, e;
-    Time(int s, int e) {
+    Time201(int s, int e) {
         this.s = s;
         this.e = e;
     }
     @Override
-    public int compareTo(Time o){
+    public int compareTo(Time201 o){
         if(this.e==o.e) return this.s-o.s;
         else return this.e-o.e;
     }
 }
 
 class IMain201 {
-    public int solution(ArrayList<Time> arr, int n){
+    public int solution(ArrayList<Time201> arr, int n){
         int cnt=0;
         Collections.sort(arr);
         int et=0;
-        for(Time ob : arr){
+        for(Time201 ob : arr){
             if(ob.s>=et){
                 cnt++;
                 et=ob.e;
@@ -32,11 +32,11 @@ class IMain201 {
         IMain201 T = new IMain201();
         Scanner kb = new Scanner(System.in);
         int n=kb.nextInt();
-        ArrayList<Time> arr = new ArrayList<>();
+        ArrayList<Time201> arr = new ArrayList<>();
         for(int i=0; i<n; i++){
             int x=kb.nextInt();
             int y=kb.nextInt();
-            arr.add(new Time(x, y));
+            arr.add(new Time201(x, y));
         }
         System.out.println(T.solution(arr, n));
     }
--- src/main/java/lecture/I09_Greedy/IMain301.java
+++ src/main/java/lecture/I09_Greedy/IMain301.java
@@ -1,26 +1,26 @@
 package lecture.I09_Greedy;
 
 import java.util.*;
-//class Time implements Comparable<Time>{
-//    public int time;
-//    public char state;
-//    Time(int time, char state) {
-//        this.time = time;
-//        this.state = state;
-//    }
-//    @Override
-//    public int compareTo(Time ob){
-//        if(this.time==ob.time) return this.state-ob.state;
-//        else return this.time-ob.time;
-//    }
-//}
+class Time301 implements Comparable<Time301>{
+    public int time;
+    public char state;
+    Time301(int time, char state) {
+        this.time = time;
+        this.state = state;
+    }
+    @Override
+    public int compareTo(Time301 ob){
+        if(this.time==ob.time) return this.state-ob.state;
+        else return this.time-ob.time;
+    }
+}
 
-class HMain301 {
-    public int solution(ArrayList<Time> arr){
+class IMain301 {
+    public int solution(ArrayList<Time301> arr){
         int answer=Integer.MIN_VALUE;
         Collections.sort(arr);
         int cnt=0;
-        for(Time ob : arr){
+        for(Time301 ob : arr){
             if(ob.state=='s') cnt++;
             else cnt--;
             answer=Math.max(answer, cnt);
@@ -29,15 +29,15 @@ class HMain301 {
     }
 
     public static void main(String[] args){
-        HMain301 T = new HMain301();
+        IMain301 T = new IMain301();
         Scanner kb = new Scanner(System.in);
         int n=kb.nextInt();
-        ArrayList<Time> arr = new ArrayList<>();
+        ArrayList<Time301> arr = new ArrayList<>();
         for(int i=0; i<n; i++){
             int sT=kb.nextInt();
             int eT=kb.nextInt();
-            arr.add(new Time(sT, 's'));
-            arr.add(new Time(eT, 'e'));
+            arr.add(new Time301(sT, 's'));
+            arr.add(new Time301(eT, 'e'));
         }
         System.out.println(T.solution(arr));
     }
