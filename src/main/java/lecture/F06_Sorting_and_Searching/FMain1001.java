8a71735b53dedb8b0707b5e70ada5619097f5b9b
jinsu291 <wlstn291@naver.com>
2022-11-01 14:23:55 +0900

    FMain1001 추가

---
+++ src/main/java/lecture/F06_Sorting_and_Searching/FMain1001.java
@@ -0,0 +1,48 @@
+package lecture.F06_Sorting_and_Searching;
+//★
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FMain1001 {
+    private int count(int[] arr, int dist) {
+        int cnt = 1;
+        int ep = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - ep >= dist) {
+                cnt++;
+                ep = arr[i];
+            }
+        }
+        return cnt;
+    }
+
+    private int solution(int n, int k, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 1;
+        int rt = arr[n - 1];
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) >= k) {
+                answer = mid;
+                lt = mid + 1;
+            } else {
+                rt = mid - 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FMain1001 T = new FMain1001();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(T.solution(n, k, arr));
+    }
+}
