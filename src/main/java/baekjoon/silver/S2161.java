c07672bcca0e644cbb5b94558ca1c86e25b89739
jinsu291 <wlstn291@naver.com>
2022-11-01 21:54:55 +0900

    S2161 추가

---
+++ src/main/java/baekjoon/silver/S2161.java
@@ -0,0 +1,33 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class S2161 {
+    private ArrayList<Integer> solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        Queue<Integer> Q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            Q.offer(i + 1);
+        }
+        while (Q.size() != 1){
+                System.out.print(Q.poll().toString() + " ");
+                Q.offer(Q.poll());
+        }
+        System.out.print(Q.poll());
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        S2161 T = new S2161();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int x : T.solution(n)) {
+            System.out.print(x);
+        }
+    }
+}
