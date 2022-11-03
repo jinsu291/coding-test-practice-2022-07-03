c5acc1d71d011bc641eb9e1c71e9f79dd332ac40
jinsu291 <wlstn291@naver.com>
2022-11-03 16:29:28 +0900

    S10773 추가

---
+++ src/main/java/baekjoon/silver/S10773.java
@@ -0,0 +1,27 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class S10773 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+        for (int o : stack) {
+            sum += o;
+        }
+        System.out.println(sum);
+    }
+}
