25fb1e24ea335ba748fe9250cf0fa8895cf9b994
jinsu291 <wlstn291@naver.com>
2022-11-04 00:51:50 +0900

    JMain201 추가

--- src/main/java/lecture/J10_DynamicProgramming/JMain201.java
+++ src/main/java/lecture/J10_DynamicProgramming/JMain201.java
@@ -2,22 +2,22 @@ package lecture.J10_DynamicProgramming;
 
 import java.util.Scanner;
 
-public class JMain101 {
+public class JMain201 {
     private static int[] dy;
     private int solution(int n) {
         dy[1] = 1;
         dy[2] = 2;
-        for (int i = 3; i <= n; i++) {
+        for (int i = 3; i <= n + 1; i++) {
             dy[i] = dy[i - 1] + dy[i - 2];
         }
-        return dy[n];
+        return dy[n + 1];
     }
 
     public static void main(String[] args) {
-        JMain101 T = new JMain101();
+        JMain201 T = new JMain201();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        dy = new int[n + 1];
+        dy = new int[n + 2];
         System.out.println(T.solution(n));
     }
 }
