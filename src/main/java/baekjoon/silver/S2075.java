12148a2135a2f4aa97ec10899fe24ebace24c283
jinsu291 <wlstn291@naver.com>
2022-11-05 20:16:34 +0900

    S2075 추가

---
+++ src/main/java/baekjoon/silver/S2075.java
@@ -0,0 +1,32 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class S2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                pq.offer(arr[i][j]);
+            }
+        }
+        for (int i = 0; i < n - 1; i++) {
+            pq.poll();
+        }
+        System.out.println(pq.poll());
+    }
+}
