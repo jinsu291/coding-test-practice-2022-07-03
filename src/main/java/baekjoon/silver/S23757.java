40bce1ec7e5e66cd70fb7bd65419b763fbbdf295
jinsu291 <wlstn291@naver.com>
2022-11-11 14:59:46 +0900

    S23757 추가

---
+++ src/main/java/baekjoon/silver/S23757.java
@@ -0,0 +1,33 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class S23757 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            pq.add(Integer.parseInt(st.nextToken()));
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int cur = Integer.parseInt(st.nextToken());
+            int max = pq.poll();
+            if (cur > max) {
+                System.out.println(0);
+                return;
+            }
+            pq.add(max - cur);
+        }
+        System.out.println(1);
+    }
+}
