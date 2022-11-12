f3527e75264ae4f1e5b31e84e4bf654e07e63508
jinsu291 <wlstn291@naver.com>
2022-11-12 20:22:04 +0900

    S9237 추가

---
+++ src/main/java/baekjoon/silver/S9237.java
@@ -0,0 +1,27 @@
+package baekjoon.silver;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class S9237 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = arr[i] + i + 1;
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max + 1);
+    }
+}
