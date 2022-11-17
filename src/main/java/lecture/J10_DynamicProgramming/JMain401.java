d2693e341eed7b6b0dec2ff4fa60ea881ee25a28
jinsu291 <wlstn291@naver.com>
2022-11-17 17:43:30 +0900

    JMain401 추가

---
+++ src/main/java/lecture/J10_DynamicProgramming/JMain401.java
@@ -0,0 +1,58 @@
+package lecture.J10_DynamicProgramming;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+//★
+
+public class JMain401 {
+    private static int[] dy;
+
+    private static class Brick implements Comparable<Brick>{
+        int s, h, w;
+
+        public Brick(int s, int h, int w) {
+            this.s = s;
+            this.h = h;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Brick o) {
+            return o.s - this.s;
+        }
+    }
+
+    private int solution(ArrayList<Brick> arr) {
+        int answer = 0;
+        Collections.sort(arr);
+        dy[0] = arr.get(0).h;
+        answer = dy[0];
+        for (int i = 0; i < arr.size(); i++) {
+            int max_h = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr.get(j).w > arr.get(i).w && dy[j] > max_h) {
+                    max_h = dy[j];
+                }
+            }
+            dy[i] = max_h + arr.get(i).h;
+            answer = Math.max(answer, dy[i]);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        JMain401 T = new JMain401();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        ArrayList<Brick> arr = new ArrayList<>();
+        dy = new int[n];
+        for (int i = 0; i < n; i++) {
+            int a = kb.nextInt();
+            int b = kb.nextInt();
+            int c = kb.nextInt();
+            arr.add(new Brick(a, b, c));
+        }
+        System.out.println(T.solution(arr));
+    }
+}
