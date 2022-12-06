9b9d7f31584e27359ac5c0c9e98b4dc1de2a01f8
jinsu291 <wlstn291@naver.com>
2022-12-06 22:56:59 +0900

    G12904 추가

---
+++ src/main/java/baekjoon/gold/G12904.java
@@ -0,0 +1,27 @@
+package baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class G12904 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer s = new StringBuffer(br.readLine());
+        StringBuffer t = new StringBuffer(br.readLine());
+
+        while (s.length() < t.length()) {
+            if (t.charAt(t.length() - 1) == 'A') {
+                t.deleteCharAt(t.length() - 1);
+            } else if (t.charAt(t.length() - 1) == 'B') {
+                t.deleteCharAt(t.length() - 1);
+                t.reverse();
+            }
+        }
+        if (s.toString().equals(t.toString())) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
