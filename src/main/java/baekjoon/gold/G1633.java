f7049f7878c57ff03b57f83c12a6cf5447f54dfa
jinsu291 <wlstn291@naver.com>
2023-01-23 23:42:34 +0900

    G1633 추가

---
+++ src/main/java/baekjoon/gold/G1633.java
@@ -0,0 +1,36 @@
+package baekjoon.gold;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class G1633 {
+    public static int[] white;
+    public static int[] black;
+    public static int[][][] dp;
+    public static void main(String args[]) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        white = new int[1001];
+        black = new int[1001];
+        int index = 0;
+        while(scan.hasNextInt()){
+            white[index] = scan.nextInt();
+            black[index] = scan.nextInt();
+
+            index++;
+        }
+        dp = new int[1001][16][16];
+        System.out.println(solution(0, 0, 0, index));
+
+    }
+    public static int solution(int i, int wIndex, int bIndex, int N){
+        if(wIndex==15 && bIndex==15) return 0;
+        if(i==N) return 0;
+
+        if(dp[i][wIndex][bIndex]!=0) return dp[i][wIndex][bIndex];
+        int ans = solution(i+1, wIndex, bIndex, N);
+        if(wIndex<15) ans = Math.max(ans, solution(i+1, wIndex+1, bIndex,N)+white[i]);
+        if(bIndex<15) ans = Math.max(ans, solution(i+1, wIndex, bIndex+1, N)+black[i]);
+        dp[i][wIndex][bIndex] = ans;
+        return dp[i][wIndex][bIndex];
+    }
+}
