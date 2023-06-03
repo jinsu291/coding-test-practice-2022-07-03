054337a36ce46f928be61227dac2eb2a034315f9
jinsu291 <wlstn291@naver.com>
2023-06-03 20:52:00 +0900

    add HMain202

--- src/main/java/lecture/H08_DFS_BFS/HMain202.java
+++ src/main/java/lecture/H08_DFS_BFS/HMain202.java
@@ -3,27 +3,19 @@ package lecture.H08_DFS_BFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
-//ㅇㅇ
-class HMain102 {
-    private static int total;
-    private static String answer = "NO";
-    private static boolean flag = false;
-    private static int n;
+class HMain202 {
+    private static int answer;
+    private static int c, n;
 
     private static void DFS(int L, int sum, int[] arr) {
-        if (flag) {
-            return;
-        }
-        if (sum > total / 2) {
+        if (sum > c) {
             return;
         }
         if (L == n) {
-            if ((total - sum) == sum) {
-                answer = "YES";
-                flag = true;
-            }
+            answer = Math.max(answer, sum);
         } else {
             DFS(L + 1, sum + arr[L], arr);
             DFS(L + 1, sum, arr);
@@ -32,13 +24,13 @@ class HMain102 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        c = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(br.readLine());
             arr[i] = num;
-            total += arr[i];
         }
         DFS(0, 0, arr);
         System.out.println(answer);
